5074839e48e58144e747ff2eecbd34f61916eef9
Sergey Kolokolchikov <sergey.bell13@gmail.com>
2026-05-03 17:47:03 +0300

    Add lattice magnetic with 5 periods in order to explan x-plane.

--- COSY/src/run/run_cosy.bat
+++ COSY/src/run/run_cosy.bat
@@ -1,15 +1,15 @@
 @echo off
 echo ========================================
-echo RUNNING: magnetic_sextupoles.fox
+echo RUNNING: magnetic_sextupoles_maps.fox
 echo ========================================
 cd ..
-cosy.exe magnetic_sextupoles.fox
+cosy.exe magnetic_sextupoles_maps.fox
 cd run
 echo.
 echo ========================================
-echo RUNNING: mapping.fox
+echo RUNNING: Twiss.fox
 echo ========================================
 cd ..
-cosy.exe mapping.fox
+cosy.exe Twiss.fox
 cd run
 echo.
